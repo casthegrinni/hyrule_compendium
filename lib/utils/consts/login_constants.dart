5760f927fba8a84622705fce18c648c5ed98810c
brunno.castigrini <brunno.castigrini@valemobi.com.br>
2025-02-22 14:24:30 -0300

    FEAT: Adicionando constantes

---
+++ lib/utils/consts/login_constants.dart
@@ -0,0 +1,5 @@
+const String errorEmpty = 'The password should not be empty.';
+const String errorSixCharacters =
+    'The password should have at least 6 characters';
+const String errorNumbers = 'The password should have numbers';
+const String errorLetters = 'The password should have letters';
---
+++ test/login_controller_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:practice_list_view/controllers/login_controller.dart';
+
+void main() {
+  final controller = LoginController();
+
+  test('Show error when password is not long enough', () {
+    expect(actual, matcher)
+  });
+
+}
